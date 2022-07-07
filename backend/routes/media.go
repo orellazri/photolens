639b028ef672fb67d35b71312b3db3fa85a56426
Orel <orel.lazri@exlibrisgroup.com>
2022-07-07 18:56:03 +0300

    Get media by id

--- backend/routes/media.go
+++ backend/routes/media.go
@@ -9,6 +9,7 @@ import (
 	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/orellazri/photolens/models"
 	"github.com/orellazri/photolens/utils"
 )
 
@@ -19,22 +20,30 @@ func RegisterMediaRouter(context *utils.Context, router *mux.Router) {
 func GetMedia(w http.ResponseWriter, r *http.Request, context *utils.Context) {
 	idStr := mux.Vars(r)["id"]
 
+	// Check that the id is a number
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
 		SendError(w, "Invalid id")
 		return
 	}
 
-	// TODO: Look for path from id in DB
-
-	img, err := os.Open(fmt.Sprintf("%s/%d.jpg", context.RootPath, id))
+	// Query path from id in database
+	var media models.Media
+	err = context.DB.First(&media, id).Error
 	if err != nil {
-		log.Printf("Could not load media %d", id)
+		log.Printf("Could not find image in database! %v", err)
+		SendError(w, "Invalid id")
+		return
+	}
+
+	img, err := os.Open(fmt.Sprintf("%s/%s", context.RootPath, media.Path))
+	if err != nil {
+		log.Printf("Could not load media %d! %v", id, err)
 		SendError(w, "Could not load media")
 		return
 	}
 
 	// TODO: Set correct content type
-	w.Header().Set("Content-Type", "image/jpeg")
+	w.Header().Set("Content-Type", media.ContentType)
 	io.Copy(w, img)
 }
