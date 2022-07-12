99e37c09cc2bf7f3b2649fc8164c0a0c70924ba5
Orel <orel.lazri@exlibrisgroup.com>
2022-07-12 21:42:53 +0300

    Frontend: Call API for thumbnail

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,24 @@
 import { useState, useEffect } from "react";
+import { Buffer } from "buffer";
 import axios from "axios";
 
 function App() {
+  const [image, setImage] = useState<string>("");
+
   useEffect(() => {
     const fetchMedia = async () => {
-      let res = await axios.get("/media/2");
-      console.log(res);
+      let res = await axios.get("/media/thumbnail/2", { responseType: "arraybuffer" });
+      setImage("data:image/png;base64," + Buffer.from(res.data, "binary").toString("base64"));
     };
 
-    fetchMedia().catch(console.error);
+    fetchMedia();
   }, []);
 
   return (
     <div>
       <h1>Photolens</h1>
       <hr />
+      <img src={image} />
     </div>
   );
 }
