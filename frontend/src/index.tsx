5768694451cd166f1390cef8408107aaeaa61dbe
Orel <orel.lazri@exlibrisgroup.com>
2022-07-20 13:37:41 +0300

    FE: Add settings link to header

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,15 +3,16 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { Container } from "@mui/system";
-import { Grid } from "@mui/material";
+import { Button, Divider, Stack } from "@mui/material";
 import ReactDOM from "react-dom/client";
 import axios from "axios";
 
 import "./index.css";
 import Logo from "./components/Logo";
 import HomePage from "./pages/HomePage";
+import SettingsPage from "./pages/SettingsPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
@@ -27,12 +28,19 @@ root.render(
     {/* Container */}
     <Container maxWidth="xl">
       {/* Header */}
-      <Grid container justifyContent="center">
+      <Stack className="header" spacing={2}>
         <Logo />
-      </Grid>
+        {/* Links */}
+        <Stack direction="row" divider={<Divider orientation="vertical" flexItem />} spacing={2}>
+          <Link to="/settings">
+            <Button variant="outlined">Settings</Button>
+          </Link>
+        </Stack>
+      </Stack>
       {/* Page */}
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/settings" element={<SettingsPage />} />
       </Routes>
     </Container>
   </BrowserRouter>
