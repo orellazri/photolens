94b3fa75b95d98e5b4346fc8540a4272e53b838e
Orel <orel.lazri@exlibrisgroup.com>
2022-07-20 13:20:05 +0300

    FE: Add container in root

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,10 +4,13 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Container } from "@mui/system";
+import { Grid } from "@mui/material";
 import ReactDOM from "react-dom/client";
 import axios from "axios";
 
 import "./index.css";
+import Logo from "./components/Logo";
 import HomePage from "./pages/HomePage";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
@@ -21,8 +24,16 @@ axios.defaults.headers.post["Content-Type"] = "application/json";
 
 root.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
+    {/* Container */}
+    <Container maxWidth="xl">
+      {/* Header */}
+      <Grid container justifyContent="center">
+        <Logo />
+      </Grid>
+      {/* Page */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Container>
   </BrowserRouter>
 );
--- frontend/src/pages/HomePage/index.tsx
+++ frontend/src/pages/HomePage/index.tsx
@@ -1,14 +1,6 @@
-import { Container } from "@mui/system";
-
 import "./style.css";
-import Logo from "../../components/Logo";
 import Gallery from "../../components/Gallery";
 
 export default function HomePage() {
-  return (
-    <Container maxWidth="xl">
-      <Logo />
-      <Gallery />
-    </Container>
-  );
+  return <Gallery />;
 }
