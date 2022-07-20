5bb74b02682241fdb8ea8ce1a33d1f1ec3188f35
Orel <orel.lazri@exlibrisgroup.com>
2022-07-20 13:46:35 +0300

    FE: Add process media button to settings page

--- frontend/src/pages/SettingsPage/index.tsx
+++ frontend/src/pages/SettingsPage/index.tsx
@@ -1,5 +1,51 @@
+import { useState } from "react";
+import { Alert, Button, Snackbar } from "@mui/material";
+import { Box } from "@mui/system";
+import axios from "axios";
+
 import "./style.css";
 
 export default function SettingsPage() {
-  return <div>Settings</div>;
+  const [error, setError] = useState<string>("");
+  const [success, setSuccess] = useState<string>("");
+
+  const handleProcessMedia = async () => {
+    try {
+      await axios.get("/media/process");
+      setSuccess("Finished processing media");
+    } catch (e) {
+      console.error("Could not process media! " + e);
+      setError("Could not process media");
+    }
+  };
+
+  return (
+    <Box>
+      <Button variant="contained" onClick={handleProcessMedia}>
+        Process media
+      </Button>
+
+      {/* Error snackbar */}
+      <Snackbar
+        open={error !== ""}
+        onClose={() => {
+          setError("");
+        }}
+        autoHideDuration={5000}
+      >
+        <Alert severity="error">{error}</Alert>
+      </Snackbar>
+
+      {/* Success snackbar */}
+      <Snackbar
+        open={success !== ""}
+        onClose={() => {
+          setSuccess("");
+        }}
+        autoHideDuration={5000}
+      >
+        <Alert severity="success">{success}</Alert>
+      </Snackbar>
+    </Box>
+  );
 }
